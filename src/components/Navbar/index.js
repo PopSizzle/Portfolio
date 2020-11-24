33133d8322c62418598acacd72c229f9be5f36a8
Sam Poppe <poppe.samuel@gmail.com>
2020-11-23 19:46:21 -0800

    working on connection of pages and navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from '@material-ui/core/Button';
+
+function Navbar() {
+return(
+  <AppBar position="static">
+  </AppBar>
+)
+}
+
+export default Navbar;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+
+function About() {
+  
+  return(
+    
+    <div>
+      <Navbar> 
+      </Navbar>
+      <img src='src\assets\profilepic.jpg' alt='Profile picture of Sam Poppe'></img>
+      <p>
+        I'm Sam Poppe, a full stack web developer looking to join a team where I can make an impact and grow my skills. I recently graduated from a coding class at the UC Berkeley Extension where I learned HTML, CSS, Javascript, React, Node, Express, MongoDB, and MySQL, among other things.
+      </p>
+    </div>
+  )
+}
+
+export default About;
